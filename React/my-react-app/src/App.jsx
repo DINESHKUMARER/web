c762b849ed98fe34b9d7a88606769372b343ba15
DINESH KUMAR ER <dk03111207@gmail.com>
2026-08-01 18:14:47 +0530

    My React App

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import CartButton from "./btn";
+
+function App(){
+
+  return(
+    <>
+     <h1>Hi guys I'm Dinesh Kumar web Developer</h1>
+
+     <p>Nothing is Possible dude</p>
+
+     <CartButton  />
+     <CartButton  />
+
+     <button>Login</button>
+
+     <CartButton  />
+     <CartButton  />
+    </>
+  )
+
+}
+export default App;
